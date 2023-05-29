bfa98608e55b83b84b72c7cab5b0afd59ad93c77
Carlos <c.cardenas@refacil.com>
2023-05-29 09:32:05 -0500

    Add the intersections endpoing and getting them from js

--- app.py
+++ app.py
@@ -17,6 +17,20 @@ def calculate_sma(df, window):
     # Remove the NaN data points
     df.dropna(subset=['sma_short', 'sma_long'], inplace=True)
 
+def calculate_sma_intersections(df):
+    intersections = []
+    sma_short = df['sma_short'].tolist()
+    sma_long = df['sma_long'].tolist()
+
+    for i in range(1, len(df)):
+        if sma_short[i-1] < sma_long[i-1] and sma_short[i] > sma_long[i]:
+            intersections.append((i, sma_short[i], 'buy'))
+        elif sma_short[i-1] > sma_long[i-1] and sma_short[i] < sma_long[i]:
+            intersections.append((i, sma_short[i], 'sell'))
+
+    return intersections
+
+
 # Routes
 
 app = Flask(__name__)
@@ -30,6 +44,15 @@ def data():
     data = df[['timestamp', 'close', 'sma_short', 'sma_long']].to_dict(orient='records')
     
     return jsonify(data)
+
+@app.route('/intersections', methods=['GET'])
+def intersections():
+    asset = request.args.get('asset')
+    window = int(request.args.get('window'))
+    df = fetch_data(asset, 500)
+    calculate_sma(df, window)
+    intersections = calculate_sma_intersections(df)
+    return jsonify(intersections)
 
 @app.route('/')
 def home():
